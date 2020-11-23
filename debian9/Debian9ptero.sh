c6c300d82924c3cc09641e51a1207c1c3f70ddfd
Bagou450 <roro35450@outlook.fr>
2020-11-23 21:01:06 +0100

    Corrige probleme avec omposer sur les nouveaux script

--- debian9/Debian9ptero.sh
+++ debian9/Debian9ptero.sh
@@ -115,7 +115,6 @@ curl -Lo panel.tar.gz https://github.com/pterodactyl/panel/releases/download/v0.
 tar --strip-components=1 -xzvf panel.tar.gz
 chmod -R 755 storage/* bootstrap/cache/
 cp .env.example .env
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php --version=1.10.16
 
