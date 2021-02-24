52ba28b2b1cf0691af2d8042b6b898c27c03db95
Bagou450 <roro35450@outlook.fr>
2021-02-24 17:27:36 +0100

    Update bashall.sh

--- bashall.sh
+++ bashall.sh
@@ -2,6 +2,6 @@
 echo "Mise a jour du script..."
 sleep 3
 rm -rf script.sh
-wget http://ressources.bagou450.com/scripts/script.sh
+wget https://raw.githubusercontent.com/Bagou450/Panel-installer-script/main/script.sh
 bash script.sh
 
